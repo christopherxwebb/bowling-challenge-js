907088a9b09d4d08ff24025beb959ec844f31b3b
Christopher <dagswebb@gmail.com>
2022-01-10 12:24:40 +0000

    First test for gutter game implemented along with correct object methods

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,15 +1,15 @@
 const Game = require('../lib/game');
 
-describe('Game Class'), () => {
+describe('Game Class', () => {
 
     beforeEach(() => {
         game = new Game();
     })
 
-    it('records a gutter game'), () => {
+    it('records a gutter game', () => {
         for (let i= 0; i < 20; i++) {
             game.roll(0);
         }
         expect(game.score()).toBe(0);
-    }
-}
+    })
+})
